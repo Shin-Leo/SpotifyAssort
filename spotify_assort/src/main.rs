2ac0f5ed1f41295c8eefaac2424bc01fe86af1e4
Leo Shin <54517654+Shin-Leo@users.noreply.github.com>
2021-12-04 10:55:05 -0800

    Added basic oauth with rspotify

--- spotify_assort/src/main.rs
+++ spotify_assort/src/main.rs
@@ -7,4 +7,35 @@ use std::{collections::HashMap, env, path::PathBuf};
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+
+    let creds = Credentials::from_env().unwrap();
+
+    let scopes = scopes!(
+        "user-top-read",
+        "user-read-recently-played",
+        "user-library-read",
+        "user-read-currently-playing",
+        "user-read-playback-state",
+        "user-read-playback-position",
+        "playlist-read-private",
+        "user-library-modify",
+        "user-modify-playback-state",
+        "playlist-modify-public",
+        "playlist-modify-private"
+    );
+
+    let oauth = OAuth::from_env(scopes).unwrap();
+
+    let mut spotify = AuthCodeSpotify::new(creds, oauth);
+
+    let url = spotify.get_authorize_url(false).unwrap();
+    spotify.prompt_for_token(&url).await.unwrap();
+
+    let token = spotify.token.lock().await.unwrap();
+    println!("Access token: {}", &token.as_ref().unwrap().access_token);
+    println!(
+        "Refresh token: {}",
+        token.as_ref().unwrap().refresh_token.as_ref().unwrap()
+    );
 }
